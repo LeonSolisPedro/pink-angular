746752a8469d6c251fa884fd14d0fa1fb4868b75
Pedro León <pedro@wintercr.com>
2021-04-09 17:05:45 -0500

    Corregido el nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   faUser = faUser
   faSignOutAlt = faSignOutAlt
+  navbarCollapsed = true;
 
   constructor(public authService: AuthService, private router: Router) { }
 
@@ -18,9 +19,13 @@ export class HeaderComponent implements OnInit {
   }
 
 
-  superLogout(){
+  superLogout() {
     this.authService.logout()
     this.router.navigate(['/login'])
+  }
+
+  toggleNavbarCollapsing() {
+    this.navbarCollapsed = !this.navbarCollapsed;
   }
 
 }
